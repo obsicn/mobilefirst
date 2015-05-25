0cf6f94359f48fd642fef728745831a62582a25e
obsicn <myang@MichaeldeMacBook-Pro.local>
2015-05-25 16:17:37 +0800

    add empty project

---
+++ NewBankNativeApp/NewBankNativeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewBankNativeApp
+//
+//  Created by Michael Yang on 5/25/15.
+//  Copyright (c) 2015 Michael Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewBankNativeApp/NewBankNativeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewBankNativeApp
+//
+//  Created by Michael Yang on 5/25/15.
+//  Copyright (c) 2015 Michael Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
